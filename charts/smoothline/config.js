1da545e09b5ba16b8a8f18262c43992b7981e2e9
U-AD001\U315546 <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2016-10-21 13:14:44 -0600

    added a file

---
+++ charts/smoothline/config.js
@@ -0,0 +1,21 @@
+
+//parse config
+
+//area charts
+
+  //necessary
+  var divId = "chartsmoothlineid";
+  var svgWidth = 400;
+  var svgHeight = 115;
+  var file = "charts/smoothline/lineyearly.tsv" ;
+
+  //optional
+  var margin = {top: 20, right: 20, bottom: 30, left: 50};
+
+  // options for time Format
+  //1)day-month-year
+  //2)month-year
+  var timeFormat ="monthYear";
+
+//  ids of checkboxes
+ // name of data column = id of checkbox?
