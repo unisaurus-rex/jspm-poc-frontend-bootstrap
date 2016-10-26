6b83fd4f8acd24a1478886da242aba6c3835ae23
Jeff Flower <jeffrey.flower@vantiv.com>
2016-10-26 17:50:37 -0600

    Finish bootstrap checkbox refactor
    Detecting bootstrap check/uncheck now handled by separate module. The
    module exposes two functions, one that adds observers to each checkbox
    and one that returns an array of values of checked checkboxes. This
    solution will not work if the bootstrap label and input elements don't
    both have name attributes with the same value.

--- scripts/bootstrapCheckboxObserver.js
+++ scripts/bootstrapCheckboxObserver.js
@@ -1,6 +1,3 @@
-
-
-
 /*
   Bootstrap checkboxes are wrapped in labels. Ex:
 
@@ -31,39 +28,79 @@
 */
 
 /*
- * @function addCheckboxObserver
+ * @function addCheckboxObservers
+ * @param cboxGropuName {string} name attribute of the bootstrap checkbox group
+          both the label and the input element need this attribute
+ * @param callback {function} function to call if the checkbox is checked or unchecked. This function must take an array of strings. These strings are the value associated with each checked input.
+ */
+export function addCheckboxObservers(cboxGroupName, callback){
+  // build css selectors for the label and for inputs of active labels
+  var labelSelector = ["label[name=", cboxGroupName, "]"].join("");
+  var inputSelector = ["label.active input[name=", cboxGroupName, "]"].join("");
+
+  $(labelSelector).each(function(idx, el){
+    addObserver(el, callback, inputSelector);
+  });
+}
+
+/*
+ * @function addObserver
  * @param el {DOM Node} a dom element (ie not a jquery object but the element
  *        a jquery object would wrap
  * @param callback {function} a function to execute when a checkbox is checked or
  *        unchecked.  The function will receive no arguments.
+ * @param inputSelector {string} css selector for choosing selected checkboxes
  * @description execute a callback function when a bootstrap checkbox is checked
  *              or unchecked
  */
-export function addCheckboxObserver(el, callback) {
+function addObserver(el, callback, inputSelector) {
+  // wrap the callback so it can be used if the mutation alters the checkbox
+  var mutationFunc = mutationFuncBuilder(callback, inputSelector);
   var observer = new MutationObserver(function(mutations){
-    mutations.forEach(function(mutation){
-      /* mutation will track the old and new value, two cases that we care about
-         1) added active class to the label
-         2) removed active class from the label
-         mutation will fire anytime a class is added or removed, the class may or may
-         not be the active class that signals a checkbox click
-         so we check to see if the active class is the class that changed betwee old and new
-      */
-      var newHasActive = mutation.target.classList.contains('active');
-      var oldHasActive = mutation.oldValue.includes('active');
-      if( (newHasActive && !oldHasActive) || (oldHasActive && !newHasActive) ){
-        callback();
-      }
-    });
+    mutations.forEach(mutationFunc);
   });
 
   // mutation observer config object, use oldValue: true so we can compare current value to old value
   // otherwise we won't be able to tell if the active value changed
   var config = {attributes: true, attributeOldValue: true, attributeFilter: ['class']};
 
+  // apply the observer to el
   observer.observe(el, config);
 
 }
 
-// function that returns function to be called by foreach, it needs to take the 
-// callback function and return a function that takes a mutation
+/*
+ * @function mutationFuncBuilder
+ * @param callback {function} function to call if checkbox is checked or unchecked
+ * @param inputSelector {string} css selector string which selects active checkboxes
+ * @returns {function}
+ * @description builds a callback function for use in array.forEach()
+ */
+function mutationFuncBuilder(callback, inputSelector) {
+  return function(mutation){
+    /* mutation will track the old and new value, two cases that we care about
+       1) added active class to the label
+       2) removed active class from the label
+       mutation will fire anytime a class is added or removed, the class may or may
+       not be the active class that signals a checkbox click
+       so we check to see if the active class is the class that changed betwee old and new
+    */
+    var newHasActive = mutation.target.classList.contains('active');
+    var oldHasActive = mutation.oldValue.includes('active');
+    if( (newHasActive && !oldHasActive) || (oldHasActive && !newHasActive) ){
+      var selectedRanges = getCheckedValues(inputSelector);
+      callback(selectedRanges);
+    }
+  }
+}
+
+/*
+ * @function getCheckedValues
+ * @param selector {string} css selector string for inputs of checked bootstrap checkboxes
+ * @description return array of values associated with checkboxes found by selector
+ * @returns array of strings
+ */
+export function getCheckedValues(selector){
+  // calling .get() on a jquery object returns a plain array of elements
+  return $(selector).get().map(function(el){return el.value;});
+}
