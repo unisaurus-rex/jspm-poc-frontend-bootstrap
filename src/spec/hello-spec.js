c53a6ae43cbc63047a170c2f1bf179328769daa9
April Hudspeth <april.hudspeth@vantiv.com>
2016-12-05 12:59:18 -0700

    add jasmine test to hello-spec.js

--- src/spec/hello-spec.js
+++ src/spec/hello-spec.js
@@ -1,5 +1,6 @@
 import addToggle from 'toggle';
- 
+import initMap from '../scripts/createLeafletMap.js'; 
+
 describe("hello world", function(){
   it("passes", function(){
     var t = true;
@@ -8,8 +9,40 @@ describe("hello world", function(){
   });
   
   it("fails", function(){
-    var f = false;
+    var f = true;
     
     expect(f).toBe(true);
   });
+});
+
+describe("test the initMap() in createLeafletMap.js", function(){
+	it("should throw an error if given null parameters", function(){
+		var mapObjectWithNullValues = {
+			parentSelector: null,
+			mapContainerId: null, 
+			mapCenter: null,
+			initialZoom: null
+		}
+		expect(function() {initMap(mapObjectWithNullValues);}).toThrowError("L is not defined");
+		
+	});	
+
+	it("should throw an error if undefined", function(){
+		var mapObjectUndefined = undefined;
+
+		expect(function () {initMap(mapObjectUndefined);}).toThrow();
+	});
+
+	it("should throw an error if undefined parameters", function(){
+		var mapObjectWithUndefinedValues = {
+			parentSelector: undefined,
+			mapContainerId: undefined, 
+			mapCenter: undefined,
+			initialZoom: undefined
+		}
+		expect(function() {initMap(mapObjectWithUndefinedValues);}).toThrowError("L is not defined");
+
+	});
+
+
 });
